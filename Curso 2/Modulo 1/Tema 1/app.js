7c224c1ed7e9adb23dd6ddbbb6296172c5fe484e
Ark <arkdan@gmail.com>
2023-03-28 22:38:43 -0600

    Curso 2

---
+++ Curso 2/Modulo 1/Tema 1/app.js
@@ -0,0 +1,16 @@
+/*
+alert("Hola mundo con JS");
+alert("Bienvenido al curso");
+*/
+// Hola pues
+/*
+console.log("muestra esto en la consola");
+console.log(26**2);
+*/
+var pais = "Mexico";
+var continente = "Amereica";
+var antiguedad = 2019;
+var pais_y_continente = pais + ", " + continente;
+
+console.log(pais, continente);
+alert(pais_y_continente);
---
+++ Curso 2/Modulo 1/Tema 1/letvar.js
@@ -0,0 +1,13 @@
+var numero = 30;
+console.log(numero);
+
+if(true) {
+    var numero = 40;
+    console.log(numero);
+}
+
+console.log(numero);
+
+var texto = "Curso de JS";
+console.log(texto);
+
