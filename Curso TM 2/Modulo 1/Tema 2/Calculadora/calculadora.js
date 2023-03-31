a93e35cba73c01149e62a4ec0167853d7b133003
Ark <arkdan@gmail.com>
2023-03-30 22:24:30 -0600

    cambios en proyecto

---
+++ Curso TM 2/Modulo 1/Tema 2/Calculadora/calculadora.js
@@ -0,0 +1,17 @@
+var numero1= ''; // 0 - 9
+var numero2= ''; // 0 - 9
+var operador= ''; // + - * /
+var resultado = '';
+
+
+function agregarNumero(numero){
+}
+
+function agregarOperador(op){
+}
+
+function calcular(){
+}
+
+function limpiar(){
+}
